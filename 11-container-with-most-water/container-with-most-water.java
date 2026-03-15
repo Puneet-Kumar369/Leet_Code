40a32b02e57d4124e0cbd9935461f973ad66e7e6
Puneet-Kumar369 <puneet071896@greenwoodhigh.edu.in>
2026-03-15 09:40:52 +0530

    Time: 5 ms (82.11%) | Memory: 77.3 MB (65.21%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int leftPointer=0,rightPointer=height.length-1,max_area=0;
+        while(leftPointer<rightPointer){
+            int width=rightPointer-leftPointer;
+            int high=Math.min(height[leftPointer],height[rightPointer]);
+            max_area=Math.max(max_area,width*high);
+            if(height[leftPointer]<height[rightPointer]){
+                leftPointer++;
+            }
+            else{
+                rightPointer--;
+            }
+        }
+        return max_area;
+        
+    }
+}
