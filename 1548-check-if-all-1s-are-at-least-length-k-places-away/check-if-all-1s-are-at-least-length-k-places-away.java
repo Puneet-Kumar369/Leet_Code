9b7ee717d4439a7811ca140babfd21b3076a1085
Puneet-Kumar369 <puneet071896@greenwoodhigh.edu.in>
2026-04-18 12:44:23 +0530

    Time: 1 ms (100.00%) | Memory: 64.7 MB (98.90%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
-        int count=0;
-        int first_one=-1;
+        int count = 0;
+        int first_one = -1;
         //int var=true;
-        int len=nums.length;
-        for(int i=0;i<len;i++){
-            if(nums[i]==1){
-                first_one=i;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == 1) {
+                first_one = i;
                 break;
             }
         }
-        for(int i=first_one+1;i<len;i++){
-                if(nums[i]==1){
-                if(count<k){return false;}
-                count=0;
-            }
-            else{
+        for (int i = first_one + 1; i < len; i++) {
+            if (nums[i] == 1) {
+                if (count < k) {
+                    return false;
+                }
+                count = 0;
+            } else {
                 count++;
             }
         }
-    return true;
+        return true;
     }
 }
 // 0101      k==2
