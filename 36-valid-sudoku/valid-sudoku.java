3789e3e497581809179ab4265fca251cde0dc873
Puneet-Kumar369 <puneet071896@greenwoodhigh.edu.in>
2026-05-03 11:35:33 +0530

    Time: 1 ms (99.90%) | Memory: 46.5 MB (57.88%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] row = new boolean[9][9];
-        boolean[][] col = new boolean[9][9];
-        boolean[][] box = new boolean[9][9];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char current = board[i][j];
-                if (current == '.') {
+        boolean[][] row=new boolean[9][9];
+        boolean[][] col=new boolean[9][9];
+        boolean[][] box=new boolean[9][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char current=board[i][j];
+                if(current=='.'){
                     continue;
                 }
-                int digitIDX = current - '0' - 1;
-                int boxIDX = (i / 3) * 3 + (j / 3);
-                if (row[i][digitIDX] || col[j][digitIDX] || box[boxIDX][digitIDX]) {
+                int digitIDX=current-'0'-1;
+                int boxIDX=(i/3)*3+(j/3);
+                if(row[i][digitIDX] || col[j][digitIDX] || box[boxIDX][digitIDX]){
                     return false;
                 }
-                row[i][digitIDX] = true;
-                box[boxIDX][digitIDX] = true;
-                col[j][digitIDX] = true;
+                row[i][digitIDX]=true;
+                box[boxIDX][digitIDX]=true;
+                col[j][digitIDX]=true;
             }
         }
         return true;
+        
 
     }
 }
