9609606ed29c893b1f8119ea73e62f89c67e19b3
Puneet-Kumar369 <puneet071896@greenwoodhigh.edu.in>
2026-05-10 14:19:17 +0530

    Time: 5 ms (98.69%) | Memory: 47.4 MB (31.71%) - LeetSync

---
+++ 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double trimMean(int[] arr) {
+        int len=arr.length;
+        int trim_len=(int)Math.round(len*(0.05));
+        Arrays.sort(arr);
+        double n=arr.length*(0.09);
+        double sum=0;
+        for(int i=trim_len;i<len-trim_len;i++){
+            sum+=arr[i];
+        }
+        
+        return (n==0)?0:(sum/n)/10;
+        
+    }
+}
